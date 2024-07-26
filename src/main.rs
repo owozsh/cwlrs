0fdc0287e0eb4e78da8a191fa9e86502e02b78f5
Vítor Barroso <contact@owozsh.dev>
2024-07-26 19:14:16 -0300

    Update template and generator

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use std::{fs, io::Write};
+use std::{collections::HashMap, fs, io::Write};
 use toml::Table;
 
+fn read_template(template_file_name: &str) -> String {
+    let template_path = format!("/home/owozsh/Developer/cwlrs/templates/{template_file_name}");
+    fs::read_to_string(&template_path).expect("Error reading the template file.")
+}
+
 fn generate_from_template(config: Table) -> String {
-    let template =
-        fs::read_to_string("./templates/vscode.json").expect("Error reading the template file.");
+    let template = read_template("vscode.json");
 
-    let mut result = String::new();
+    let mut result = String::from(&template);
 
-    for (key, value) in config["base"].as_table().expect("Missing base key").iter() {
-        let from = format!("${key}");
-        dbg!(&from);
-        let to = value.as_str().expect(&format!("Missing {key} key."));
-        dbg!(&to);
-        result = template.replace(&from, &to);
+    let color_scopes = ["base", "colors", "syntax", "ui"];
+    let mut colors = HashMap::new();
+
+    for scope in color_scopes {
+        let err = format!("Missing {scope} key");
+        let scope_items = config[scope].as_table().expect(&err).iter();
+
+        for (key, value) in scope_items {
+            colors.insert(key, value);
+            let from = format!("${key}");
+            let to = String::from(value.as_str().unwrap());
+
+            dbg!(&to);
+
+            result = match &to[..1] {
+                "#" => result.replace(&from, &to),
+                _ => {
+                    let color = colors.get(&to).unwrap().as_str().unwrap();
+                    result.replace(&from, &color)
+                }
+            };
+        }
     }
 
     result
